e476d8950e46bcc165878a6f8d3dff48cf74bf12
LLCampos <luis.filipe.lcampos@gmail.com>
2021-07-11 08:46:19 +0100

    add counts

--- data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
+++ data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
@@ -12,6 +12,7 @@ object GenerateProcessedFiles extends App {
         .map(_.cadProfissao)
         .map(_.getOrElse("NULL"))
         .map(ProfessionNormalizer.normalize)
+        .groupBy(identity).view.mapValues(_.size)
         .foreach(d => println(s"${d}"))
     case Left(err) => println(err)
   }
