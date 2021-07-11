2d89323dddf1e2e0615b6549313f38d6e5ad13ef
LLCampos <luis.filipe.lcampos@gmail.com>
2021-07-11 09:24:42 +0100

    agglomerate less represented professions in "other"

--- data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
+++ data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
@@ -6,14 +6,24 @@ object GenerateProcessedFiles extends App {
 
   val filePath = "../raw-data/RegistoBiograficoXIV.json"
 
+  def groupProfessionsByCategory(professions: Seq[String]): Map[String, Int] = {
+    val (main, others) = professions
+      .map(ProfessionNormalizer.normalize)
+      .groupBy(identity)
+      .view
+      .mapValues(_.size)
+      .partition(_._2 > 1)
+    main.toMap + ("Others" -> others.values.sum)
+  }
+
   RawDeputadoParser.getRawDeputados(filePath) match {
     case Right(deputados) =>
-      deputados
+      val professions = deputados
         .map(_.cadProfissao)
         .map(_.getOrElse("NULL"))
-        .map(ProfessionNormalizer.normalize)
-        .groupBy(identity).view.mapValues(_.size).toSeq
-        .sortBy(_._2).reverse
+      groupProfessionsByCategory(professions).toSeq
+        .sortBy(_._2)
+        .reverse
         .foreach(d => println(s"${d}"))
     case Left(err) => println(err)
   }
