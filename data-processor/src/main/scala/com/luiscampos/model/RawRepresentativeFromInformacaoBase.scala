eadd3928b1d1a0b5e05f626a4d1ae0c10bd28cb9
LLCampos <luis.filipe.lcampos@gmail.com>
2021-07-17 09:39:25 +0100

    implement fromInformacaoBaseFile

--- data-processor/src/main/scala/com/luiscampos/model/RawRepresentativeFromInformacaoBase.scala
+++ data-processor/src/main/scala/com/luiscampos/model/RawRepresentativeFromInformacaoBase.scala
@@ -1,12 +1,34 @@
 package com.luiscampos.model
 
+import com.luiscampos.util.FileUtil
 import io.circe
-
+import io.circe.Decoder
+import io.circe.DecodingFailure
+import io.circe.Json
+import io.circe.generic.semiauto._
 
 final case class RawRepresentativeFromInformacaoBase(
   depCadId: RepresentativeId
 )
 
 object RawRepresentativeFromInformacaoBase {
-    def fromInformacaoBaseFile(filePath: String): Either[circe.Error, Seq[RawRepresentativeFromInformacaoBase]] = ???
+
+  implicit val fooDecoder: Decoder[RawRepresentativeFromInformacaoBase] =
+    deriveDecoder[RawRepresentativeFromInformacaoBase]
+
+  def fromInformacaoBaseJson(json: Json): Either[DecodingFailure, Seq[RawRepresentativeFromInformacaoBase]] =
+    json.hcursor
+      .downField("Legislatura")
+      .downField("Deputados")
+      .downField("pt_ar_wsgode_objectos_DadosDeputadoSearch")
+      .focus
+      .get
+      .as[Seq[RawRepresentativeFromInformacaoBase]]
+
+  def fromInformacaoBaseFile(filePath: String): Either[circe.Error, Seq[RawRepresentativeFromInformacaoBase]] =
+    FileUtil
+      .fileToJson(filePath)
+      .flatMap(json => {
+        RawRepresentativeFromInformacaoBase.fromInformacaoBaseJson(json)
+      })
 }
