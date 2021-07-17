718557d179023df83a22c961bb1e7ccf2be22319
LLCampos <luis.filipe.lcampos@gmail.com>
2021-07-17 10:11:08 +0100

    fix csv generation

--- data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
+++ data-processor/src/main/scala/com/luiscampos/GenerateProcessedFiles.scala
@@ -2,6 +2,9 @@ package com.luiscampos
 
 import com.luiscampos.model._
 
+import java.io.File
+import java.io.PrintWriter
+
 object GenerateProcessedFiles extends App {
 
   def registoBiograficoFilePath(legislature: String) =
@@ -56,37 +59,37 @@ object GenerateProcessedFiles extends App {
   }.flatten
 
   // legislatures.foreach { l =>
-  //   println(l.legislatureNumber)
+  //   println(l.number)
   //   println()
   //   l.professions.toSeq.sortBy(_._2).reverse.foreach(println)
   //   println("--------------------")
   // }
 
-//   writeToFile("../processed-data/professions.csv", toCsv(legislatures))
+  writeToFile("../processed-data/professions.csv", toCsv(legislatures))
 
-//   private def writeToFile(filePath: String, content: String): Unit = {
-//     val pw = new PrintWriter(new File(filePath))
-//     pw.write(content)
-//     pw.close
-//   }
+  private def writeToFile(filePath: String, content: String): Unit = {
+    val pw = new PrintWriter(new File(filePath))
+    pw.write(content)
+    pw.close
+  }
 
-//   private def toCsv(legislatures: Seq[Legislature]): String = {
-//     val columnNames = "legislatureNumber,profession_cat,number_of_deputados"
-//     val rows = legislatures
-//       .flatMap { legislature =>
-//         legislature.professions.map { case (profession_cat, n) =>
-//           s"${legislature.number},$profession_cat,$n"
-//         }
-//       }
-//       .mkString("\n")
-//     columnNames + "\n" + rows
-//   }
+  private def toCsv(legislatures: Seq[Legislature]): String = {
+    val columnNames = "legislatureNumber,profession_cat,number_of_deputados"
+    val rows = legislatures
+      .flatMap { legislature =>
+        legislature.professions.map { case (profession_cat, n) =>
+          s"${legislature.number},$profession_cat,$n"
+        }
+      }
+      .mkString("\n")
+    columnNames + "\n" + rows
+  }
 
   private def groupProfessionsByCategory(
     professions: Seq[String]
   ): Map[String, Int] = {
     val (main, others) =
-      professions.map(ProfessionNormalizer.normalize).groupBy(identity).view.mapValues(_.size).partition(_._2 > 1)
+      professions.groupBy(identity).view.mapValues(_.size).partition(_._2 > 1)
     // others.toMap.keys.foreach(println)
     main.toMap + ("Others" -> others.values.sum)
   }
